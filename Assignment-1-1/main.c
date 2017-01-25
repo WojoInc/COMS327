5c5343e70bd562fb2b03affb9b5378eaff8ca888
wojoinc <wojoinc@gmail.com>
2017-01-25 00:32:39 -0600

    Implemented room generation. Fixed issue with dungeon not correctly outputting to console. Migrated all aspects of world units within the dungeon struct into a w_unit struct rather than an enum, as this caused issues and made code sloppy.

--- Assignment-1-1/main.c
+++ Assignment-1-1/main.c
@@ -4,51 +4,89 @@
 
 #include "main.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 
 int main(int argc, char *argv[]){
 
     //dungeon variable, an struct storing the weights of each square in the dungeon
-    dungeon_t dungeon;
-    //array representing rooms present in the dungeon, for now i1 = height, i2 = width
-    int rooms[10][10];
-    setBoundaries(dungeon.wunits);
-    for (int i = 0; i < 10; ++i) {
+    dungeon_t dungeon= {{0,0,0,0},{ROCK,0}};
 
-    }
-    printDungeon(dungeon.wunits);
+    setBoundaries(&dungeon);
+    //printDungeon(&dungeon);
+    placeRooms(&dungeon);
+    printDungeon(&dungeon);
 
 
 }
 
-/**
- * Sets the weight of the cells at the edges of the dungeon to IMPASS, effectively
- * creating boundaries which rooms, corridors, the PC, and NPCs may not pass or rest on.
- * @param dungeon the dungeon to draw boundaries on.
- */
-void setBoundaries(wunit_state_t dungeon[d_HEIGHT][d_WIDTH]){
+void applyRoom(room_t *room, dungeon_t *dungeon){
+    for (int i = 0; i < room->height; ++i) {
+        for (int j = 0; j < room->width; ++j) {
+            dungeon->wunits[room->y+i][room->x+j].type = rm_FLOOR;
+        }
+    }
+}
+
+bool checkRoom(room_t *room, dungeon_t *dungeon){
+
+    //check to make sure there is a 1 world unit gap around the area the room would cover
+    for (int i = -1; i <= room->height; ++i) {
+        //check each row verify world units are either ROCK or a CORRIDOR
+        for (int j = -1; j < room->width; ++j) {
+            if(dungeon->wunits[room->y+i][room->x+j].type!=ROCK) return false;
+        }
+    }
+    return true;
+}
+
+void placeRooms(dungeon_t *dungeon){
+
+    room_t temp;
+    int tries;
+    //TODO find better method for randomly choosing a number from a range
+    for (int count = 0; count < NUM_ROOMS; ++count) {
+        //printDungeon(dungeon);
+        temp.y = (rand()%d_HEIGHT-1)+1;
+        temp.x = (rand()%d_WIDTH-1)+1;
+        temp.height = (rand()%r_MIN_H-1)+7;
+        temp.width = (rand()%r_MIN_W-1)+7;
+
+        tries = 0;
+
+        while (checkRoom(&temp, dungeon) == false && tries < MAX_TRIES) {
+            temp.y = (rand() % d_HEIGHT - 1) + 1;
+            temp.x = (rand() % d_WIDTH - 1) + 1;
+            temp.height = (rand() % r_MIN_H - 1) + 7;
+            temp.width = (rand() % r_MIN_W - 1) + 7;
+            tries++;
+        }
+        if (tries < MAX_TRIES) { //meaning we did not time out, and room is valid
+            dungeon->rooms[count] = temp;
+            applyRoom(&temp,dungeon);
+        }
+    }
+}
+
+void setBoundaries(dungeon_t *dungeon){
     //sides
     for (int i = 0; i < d_HEIGHT; ++i) {
-        dungeon[i][0] = IMPASS;
-        dungeon[i][d_WIDTH-1] = IMPASS;
+        dungeon->wunits[i][0].type = IMPASS;
+        dungeon->wunits[i][d_WIDTH-1].type = IMPASS;
     }
     //top and bottom
     for (int j = 0; j < d_WIDTH; ++j) {
-        dungeon[0][j] = IMPASS;
-        dungeon[d_HEIGHT-1][j] = IMPASS;
+        dungeon->wunits[0][j].type = IMPASS;
+        dungeon->wunits[d_HEIGHT-1][j].type = IMPASS;
     }
 }
 
-/**
- * Prints the dungeon via stdOut to the console
- * @param dungeon the dungeon to be displayed
- */
-void printDungeon(wunit_state_t dungeon[d_HEIGHT][d_WIDTH]){
+void printDungeon(dungeon_t *dungeon){
+
     for (int i = 0; i < d_HEIGHT; ++i) {
         for (int j = 0; j < d_WIDTH; ++j) {
 
-            switch(dungeon[i][j]){
+            switch(dungeon->wunits[i][j].type){
 
                 case IMPASS:
                     printf("%c",'$');
@@ -58,10 +96,13 @@ void printDungeon(wunit_state_t dungeon[d_HEIGHT][d_WIDTH]){
                     break;
                 case rm_FLOOR:
                     printf("%c",'-');
+                    break;
                 case ROCK:
                     printf("%c",' ');
+                    break;
                 default:
-                    printf("%c",' ');
+                    printf("%c",'x');//just to make it easier to spot errors
+                    break;
             }
         }
         printf("%c",'\n');
--- Assignment-1-1/main.h
+++ Assignment-1-1/main.h
@@ -2,48 +2,85 @@
 // Created by 97wes on 1/21/2017.
 //
 
+#include <stdint.h>
+
 #ifndef COMS327_MAIN_H
 #define COMS327_MAIN_H
 
 #endif //COMS327_MAIN_H
 
-#define d_HEIGHT 105
-#define d_WIDTH 160
+#define d_HEIGHT 160
+#define d_WIDTH 105
 
 #define r_MIN_W 7
 #define r_MIN_H 5
 
 #define NUM_ROOMS 10
+#define MAX_TRIES 2000
 
-typedef enum wunit_state{
-    IMPASS = 99,
-    ROCK = 10,
-    CORRIDOR = 5,
-    DIAMOND = 4,
-    STONE = 3,
-    DIRT = 2,
-    rm_FLOOR = 98,
-}wunit_state_t;
+
 typedef int bool;
 #define true 1
 #define false 0
 
-typedef struct wunit{
-    int state;
-}wunit_t;
+typedef enum wunit_type{
+    IMPASS = 255,
+    ROCK = 0,
+    CORRIDOR = 2,
+    rm_FLOOR = 1,
+}wunit_type_t;
+
+typedef struct w_unit{
+    wunit_type_t type;
+    int hardness;
+}w_unit_t;
 
 typedef struct room{
-    wunit_t *cells[r_MIN_H][r_MIN_W];
-    wunit_t topLeft;
+    //x and y coordinates representing the top left corner of the room
+    int y;
+    int x;
     int width;
     int height;
 }room_t;
 
 typedef struct dungeon{
     room_t rooms[NUM_ROOMS];
-    wunit_state_t wunits[d_HEIGHT][d_WIDTH];
+    w_unit_t wunits[d_HEIGHT][d_WIDTH];
 }dungeon_t;
 
+//TODO implement functions for connecting rooms
 
-void setBoundaries(wunit_state_t dungeon[][d_WIDTH]);
-void printDungeon(wunit_state_t dungeon[][d_WIDTH]);
+/**
+ * Treats the @room as a layer, then flattens this layer onto the dungeon layer. This is done by setting the value
+ * of any world unit in the dungeon that is within the area the room encompasses to wunit_state.ROOM.
+ * @param room pointer to the room to flatten
+ * @param dungeon the dungeon to apply room to
+ */
+void applyRoom(room_t *room, dungeon_t *dungeon);
+
+/**
+ * Verify if room is able to be placed at the desired location in the dungeon
+ * @param room the room to check
+ * @param dungeon the dungeon to check against
+ * @return is room placement valid
+ */
+bool checkRoom(room_t *room, dungeon_t *dungeon);
+
+/**
+ * Place a number of rooms in the dungeon specified by NUM_ROOMS
+ * @param dungeon the dungeon for rooms to be placed in to
+ */
+void placeRooms(dungeon_t *dungeon);
+
+/**
+ * Sets the weight of the cells at the edges of the dungeon to IMPASS, effectively
+ * creating boundaries which rooms, corridors, the PC, and NPCs may not pass or rest on.
+ * @param dungeon the dungeon to draw boundaries on.
+ */
+void setBoundaries(dungeon_t *dungeon);
+
+/**
+ * Prints the dungeon via stdOut to the console
+ * @param dungeon the dungeon to be displayed
+ */
+void printDungeon(dungeon_t *dungeon);
