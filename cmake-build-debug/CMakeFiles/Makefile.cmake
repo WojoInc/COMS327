a7e9f7340045f14047189032a07577ed84494d97
Thomas John Wesolowski <wojoinc@iastate.edu>
2017-02-13 09:22:13 -0600

    Began project from scratch again, implemented priority queue using simple min heap. Need to iron out issues with multiple successive removals from the heap. Could make heap faster in the future by adding alpha balancing, but not needed for now.

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Assignment-1.02.dir/DependInfo.cmake"
+  "CMakeFiles/Assignment-1.03.dir/DependInfo.cmake"
   )
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/wojoinc/CLionProjects/COMS327
+# Install script for directory: /home/wojoinc/COMS327
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/wojoinc/CLionProjects/COMS327/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/wojoinc/COMS327/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
